73e913d1d2ebdd16772da75b45aec6210d491e23
NimaSattari <54527075+NimaSattari@users.noreply.github.com>
2020-02-27 20:25:04 +0330

    Initial commit

---
+++ Assets/Answers.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Answers : MonoBehaviour
+{
+    [SerializeField] bool answer = false;
+    [SerializeField] GameObject Lose;
+    int currentSceneIndex;
+
+    private void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+    }
+
+    public void Anser()
+    {
+        if (answer)
+        {
+            StartCoroutine(KirKhar());
+        }
+
+        else
+        {
+            Lose.SetActive(true);
+            StartCoroutine(KirKhar());
+        }
+    }
+
+    IEnumerator KirKhar()
+    {
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}
